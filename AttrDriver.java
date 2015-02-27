777075e92d24ce5dc25d914fbd2d48859f1c1e2c
kylelowtharp <klv9700@saintjoe.edu>
2015-02-27 16:08:07 -0500

    Attr Driver#

---
+++ AttrDriver.java
@@ -0,0 +1,9 @@
+
+
+public class AttrDriver {
+  public static void manin (String[] args) {
+	//create objects of ColorAttr
+	ColorAttr color1 = new ColorAttr("Red", "Crimson");
+	Systme.out.println(color1.toString());
+}
+}
